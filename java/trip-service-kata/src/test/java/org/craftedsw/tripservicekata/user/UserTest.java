1be874a6c769c104346c595629923fb6470313b4
cgilet <cgilet@octo.com>
2017-02-26 13:47:38 +0100

    Add method isFriendsWith(User) in User class

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,36 @@
 package org.craftedsw.tripservicekata.user;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.craftedsw.tripservicekata.trip.UserBuilder.aUser;
+
 public class UserTest {
 
+    private static final User BOB = new User();
+    private static final User PAUL = new User() ;
+
+    @Test
+    public void shouldReturnFalse_whenNotFriends() {
+        // Given
+        User user = aUser().friendsWith(BOB).build();
+
+        // When
+        boolean result = user.isFriendsWith(PAUL);
+
+        // Then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void shouldReturnTrue_whenNotFriends() {
+        // Given
+        User user = aUser().friendsWith(BOB, PAUL).build();
+
+        // When
+        boolean result = user.isFriendsWith(PAUL);
+
+        // Then
+        assertThat(result).isTrue();
+    }
 }
